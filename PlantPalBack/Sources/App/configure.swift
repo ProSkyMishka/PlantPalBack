0a7e48bb8f996d6739004eb728446d2d4f23dbd7
ProSkyMishka <145161036+ProSkyMishka@users.noreply.github.com>
2024-07-19 00:35:27 +0300

    Update configure.swift

--- PlantPalBack/Sources/App/configure.swift
+++ PlantPalBack/Sources/App/configure.swift
@@ -8,7 +8,7 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.http.server.configuration.address = .hostname("0.0.0.0", port: 8080)
+    app.http.server.configuration.address = .hostname("51.250.35.10", port: 8080)
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "51.250.35.10",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
